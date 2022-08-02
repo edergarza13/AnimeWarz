000be0d7e933168ba0cd7a93d828a6aa7b4e8fe7
Eder Garza <edergarza@Eders-MacBook-Pro.local>
2022-08-01 21:57:01 -0500

    adding character and teams files

---
+++ AnimeWarz/character.py
@@ -0,0 +1,7 @@
+class Character:
+    def __init__(self, name, anime, attack, defense, health):
+        self.name = name
+        self.anime = anime
+        self.attack = attack
+        self.defense = defense
+        self.health = health
---
+++ AnimeWarz/teams.py
@@ -0,0 +1,17 @@
+class Team:
+    def __init__(self, char_one, char_two, char_three):
+        self.char_one = char_one
+        self.char_two = char_two
+        self.char_three = char_three
+
+    #CHARACTER ATTACKS BOSS
+    def attack(self, character, boss):
+        pass
+
+    #CHARACTER DEFENDS ATTACK FROM BOSS
+    def defend(self, character, boss):
+        pass
+
+    #CHARACTER USES SUPPORT ITEAM
+    def support(self, char_one, char_two):
+        pass
